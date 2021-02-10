08da74a22a74795c5945c9c40ad6ea7362a6bd4c
Edouard Belfond <edouard.belfond@grenoble-em.com>
2021-02-10 16:46:05 +0100

    exo 3 en partie

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+4.times do
+  @flat = Flat.new(
+    name:    Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    description: Faker::PhoneNumber.phone_number,
+    price_per_night:  ["chinese", "italian", "japanese", "french", "belgian"].sample
+    number_of_guests:
+  )
+end
